db2aba545da6b8afdc7bfab9821e5a50fd660b83
Kevin Day <kevin.ch.day@gmail.com>
2019-10-01 21:03:19 -0500

    commit 10/1/19

--- authenticate.php
+++ authenticate.php
@@ -1,5 +1,6 @@
 <?php
 require_once('includes\header.inc');
+$refreshTime = 1;
 
 // Define variables and initialize with empty values
 $username = $password = "";
@@ -22,41 +23,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     } else{
         $password = trim($_POST["password"]);
     }
-
-    // check if errors happened
-    if(empty($username_err) && empty($password_err)){
-        // select username and password from database
-        $sql = "select username, password from users";
-
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-
-            while($row = $result->fetch_assoc()) {
-
-                if(strcmp($row["username"], $_POST["username"])){
-                    
-                    //if(strcmp($row["password"], $_POST["password"])){
-                    if($row["password"] == $_POST["password"]){
-                        $msg = "access granted";
-                    }
-                }else{
-                    $msg = "access denied";
-                }
-            }
-        }else{
-            // user table is empty
-        }
+    
+}
+if(!$_POST["username"] && !$_POST["password"]){
+?>
+    <div id="main">
+        <p>Error: No username or password</p>
+        </div>
+    </div>
+<?php
+}else{
+    if(authentication($_POST["username"], $_POST["password"])){
+        echo "<h1>Access Granted!!</h1>";
+        header('Refresh: '.$refreshTime.'; URL = private\admin.php');
     }else{
-        // errors happened
+        echo "<h1>Bad username and password</h1>";
+        header('Refresh: '.$refreshTime.'; URL = login.php');
     }
 }
-?>
-
-<div id="main">
-    <p><?php echo $msg; ?></p>
-</div>
-
-<?php
 require_once('includes\footer.inc');
 ?>
---
+++ private/admin/admin.php
@@ -0,0 +1,18 @@
+<?php
+require_once('..\..\includes\header.inc');
+?>
+
+<div id="main">
+    <h1>Admin Page</h1>
+    <p>Admin Stuff</p>
+    <ul>
+        <li><a href="assests.php">View Assests</a></li>
+        <li><a href="purchases.php">View Purchases</a></li>
+        <li><a href="deployment.php">View Deployment</a></li>
+        <li><a href="repairs.php">View Repairs</a></li>
+    </ul>
+</div>
+
+<?php
+require_once('..\..\includes\footer.inc');
+?>
